a3cb1b68d85384d0ad19d0571507874a891a6a00
anesscg <cgascovanessa@gmail.com>
2023-04-09 21:08:04 +0200

    Primer commit

---
+++ UF2Notas/src/Notas.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+/**
+ * @author aness
+ *
+ */
+
+
+import java.util.Scanner;
+
+
+public class Notas {
+  
+	double uf1, uf2, uf3;
+	double acu1, acu2, acu3, def;
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	
+	
+	public void IngresaNotas() {
+		
+		System.out.println("ingrese las notas del estudiante");
+		
+		System.out.print("ingrese nota 1: ");
+		
+		uf1= entrada.nextDouble();
+
+		
+		
+		System.out.print("ingrese nota 2: ");
+		
+		uf2= entrada.nextDouble();
+		System.out.print("ingrese nota 3: ");
+		
+		uf3= entrada.nextDouble();
+		
+	
+	}
+	
+
+        
+        
+    
+	
+	public void comprobarcion(){
+		
+		if (uf1>10) {
+			System.out.println(" nota1 mal introducida");
+			
+		}else {
+			System.out.println(" nota1 correcta");
+		}
+		
+		if (uf2>10) {
+			System.out.println(" nota2 mal introducida");
+			
+		}else {
+			System.out.println(" nota2 correcta");
+		}
+		if (uf3>10) {
+			System.out.println(" nota3 mal introducida");
+			
+		}else {
+			System.out.println(" nota3 correcta");
+		}
+		
+			
+		
+		
+	}
+	
+	
+	public void Calculonotas() {
+		acu1= uf1*0.35;
+		acu2 = uf2 * 0.35;
+		acu3 = uf3 * 0.30;
+		
+		def = acu1 + acu2+ acu3;
+		
+
+	}
+       
+	public void Mostrar() {
+	
+		System.out.println(" notas introducidas son:");
+		System.out.println(" nota1 = " + uf1);
+		System.out.println(" nota2 = " + uf2);
+		System.out.println(" nota3 = " + uf3);
+		
+		System.out.println(" acumuado 1 = "+ acu1);
+		System.out.println(" acumuado 2 = "+ acu2);
+		System.out.println(" acumuado 3 = "+ acu3);
+		
+		System.out.println(" nota definitiva es = "+ def);
+		
+	}
+     
+	public void aprobado() {
+			
+			if(def<5 && def>=0) {
+				System.out.println("suspendio");
+			}else {
+				if (def>=5 && def<=10 ) {
+				System.out.println("aprobado");
+				}else {
+					System.out.println(" error en la notas");
+				}
+			}
+		}
+		
+		
+	public static void main(String[] args) {
+	
+		Notas fc= new Notas();
+		
+		fc.IngresaNotas();
+		
+		fc.comprobarcion();
+		
+
+		fc.Calculonotas();
+		
+		fc.Mostrar();
+		
+		fc.aprobado();
+		
+	}
+
+}
